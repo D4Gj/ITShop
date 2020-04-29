3f9dd29ebbc0bd8e59ce1113546dda75a30f45d7
Дмитрий Лебедев <inzadimonax@gmail.com>
2020-04-29 21:38:28 +0400

    Started

---
+++ ITShopBusinessLogic/ITShopBusinessLogic/BindingModels/OrderBindingModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ITShopBusinessLogic.BindingModels
+{
+    public class OrderBindingModel
+    {
+        public int? Id { get; set; }
+        public int? ClientId { get; set; }
+        public decimal Sum { get; set; }
+
+
+    }
+}
---
+++ ITShopBusinessLogic/ITShopBusinessLogic/BindingModels/ProductBindingModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ITShopBusinessLogic.BindingModels
+{
+    public class ProductBindingModel
+    {
+        public int? Id { get; set; }
+      //public int SpecificationsId {get;set;}
+        public string Name { get; set; }
+        public DateTime ReleaseYear { get; set; }
+        public DateTime WarrantyEnd { get; set; }
+        public double ProductPrice { get; set; }
+    }
+}
