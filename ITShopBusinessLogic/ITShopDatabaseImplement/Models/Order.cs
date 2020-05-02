b18ae118efad96a91d7c9dbae122d0b89e0ac120
KDANext <dima.1998.gig@mail.ru>
2020-05-03 01:26:31 +0400

    Update Order.cs

--- ITShopBusinessLogic/ITShopDatabaseImplement/Models/Order.cs
+++ ITShopBusinessLogic/ITShopDatabaseImplement/Models/Order.cs
@@ -7,7 +7,8 @@ namespace ITShopDatabaseImplement.Models
     public class Order
     {
         public int? Id { get; set; }
-        public int? ProductId { get; set; }        
+        public int? ProductId { get; set; }      
+        public int? ClientId { get; set; }
         public int Count { get; set; }
         public decimal Sum { get; set; }
         public DateTime OrderDate { get; set; }
