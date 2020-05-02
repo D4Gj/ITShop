fdb619b5657c07b7d5cf9b6220c505a3d3e7f8e5
Дмитрий Лебедев <inzadimonax@gmail.com>
2020-05-03 01:27:45 +0400

    created ClientLogic renamed clientlogic(notcorrect)

--- ITShopBusinessLogic/ITShopDatabaseImplement/Implements/ClientLogic.cs
+++ ITShopBusinessLogic/ITShopDatabaseImplement/Implements/ClientLogic.cs
@@ -9,7 +9,70 @@ using ITShopDatabaseImplement.Models;
 
 namespace ITShopDatabaseImplement.Implements
 {
-    public class ClientLogic
+    public class ClientLogic:IClientLogic
     {
+        public void CreateOrUpdate(ClientBindingModel model)
+        {
+            using (var context = new ITShopDatabase())
+            {
+                Client client;
+                if (model.Id.HasValue)
+                {
+                    client = context.Clients.FirstOrDefault(rec => rec.Id == model.Id);
+                    if (client == null)
+                    {
+                        throw new Exception("Элемент не найден");
+                    }
+                }
+                else
+                {
+                    client = new Client();
+                    context.Clients.Add(client);
+                }
+                client.Login = model.Login;
+                client.FirstName = model.FirstName;
+                client.LastName = model.LastName;
+                client.Password = model.Password;
+                client.Phone = model.Phone;
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(ClientBindingModel model)
+        {
+            using (var context = new ITShopDatabase())
+            {
+                Client element = context.Clients.FirstOrDefault(rec => rec.Id == model.Id);
+
+                if (element != null)
+                {
+                    context.Clients.Remove(element);
+                    context.SaveChanges();
+                }
+                else
+                {
+                    throw new Exception("Элемент не найден");
+                }
+            }
+        }
+
+        public List<ClientViewModel> Read(ClientBindingModel model)
+        {
+            using(var context = new ITShopDatabase())
+            {
+                return context.Clients
+                .Where(rec => model == null || rec.Id == model.Id ||
+                rec.Login == model.Login && rec.Password == model.Password)
+                .Select(rec => new ClientViewModel
+                {
+                    Id = rec.Id,
+                    FirstName = rec.FirstName,
+                    LastName = rec.LastName,                    
+                    Login = rec.Login,
+                    Password = rec.Password
+                })
+                .ToList();
+            }
+        }
     }
 }
