95880971429aa1a7d9cc1a55c966782508e0424e
KDANext <dima.1998.gig@mail.ru>
2020-05-08 08:15:05 +0400

    Update FormMain

--- ITShopBusinessLogic/ITShopWindowsApp/FormMain.Designer.cs
+++ ITShopBusinessLogic/ITShopWindowsApp/FormMain.Designer.cs
@@ -32,28 +32,30 @@
             this.dataGridView = new System.Windows.Forms.DataGridView();
             this.menuStrip2 = new System.Windows.Forms.MenuStrip();
             this.справочникToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.заготовкиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.КомпонентыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.изделияToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.клиентыToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buttonUpd = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             this.menuStrip2.SuspendLayout();
             this.SuspendLayout();
             // 
             // buttonCreateOrder
             // 
-            this.buttonCreateOrder.Location = new System.Drawing.Point(1300, 36);
+            this.buttonCreateOrder.Location = new System.Drawing.Point(595, 27);
             this.buttonCreateOrder.Name = "buttonCreateOrder";
             this.buttonCreateOrder.Size = new System.Drawing.Size(157, 21);
             this.buttonCreateOrder.TabIndex = 10;
             this.buttonCreateOrder.Text = "Создать заказ";
             this.buttonCreateOrder.UseVisualStyleBackColor = true;
+            this.buttonCreateOrder.Click += new System.EventHandler(this.buttonCreateOrder_Click);
             // 
             // dataGridView
             // 
             this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView.Location = new System.Drawing.Point(12, 27);
             this.dataGridView.Name = "dataGridView";
-            this.dataGridView.Size = new System.Drawing.Size(1344, 411);
+            this.dataGridView.Size = new System.Drawing.Size(577, 411);
             this.dataGridView.TabIndex = 9;
             // 
             // menuStrip2
@@ -62,48 +64,63 @@
             this.справочникToolStripMenuItem});
             this.menuStrip2.Location = new System.Drawing.Point(0, 0);
             this.menuStrip2.Name = "menuStrip2";
-            this.menuStrip2.Size = new System.Drawing.Size(1472, 24);
+            this.menuStrip2.Size = new System.Drawing.Size(765, 24);
             this.menuStrip2.TabIndex = 8;
             this.menuStrip2.Text = "menuStrip2";
             // 
             // справочникToolStripMenuItem
             // 
             this.справочникToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.заготовкиToolStripMenuItem,
+            this.КомпонентыToolStripMenuItem,
             this.изделияToolStripMenuItem,
             this.клиентыToolStripMenuItem});
             this.справочникToolStripMenuItem.Name = "справочникToolStripMenuItem";
             this.справочникToolStripMenuItem.Size = new System.Drawing.Size(87, 20);
             this.справочникToolStripMenuItem.Text = "Справочник";
             // 
-            // заготовкиToolStripMenuItem
+            // КомпонентыToolStripMenuItem
             // 
-            this.заготовкиToolStripMenuItem.Name = "заготовкиToolStripMenuItem";
-            this.заготовкиToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.заготовкиToolStripMenuItem.Text = "Заготовки";
+            this.КомпонентыToolStripMenuItem.Name = "КомпонентыToolStripMenuItem";
+            this.КомпонентыToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.КомпонентыToolStripMenuItem.Text = "Компоненты";
+            this.КомпонентыToolStripMenuItem.Click += new System.EventHandler(this.КомпонентыToolStripMenuItem_Click);
             // 
             // изделияToolStripMenuItem
             // 
             this.изделияToolStripMenuItem.Name = "изделияToolStripMenuItem";
             this.изделияToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.изделияToolStripMenuItem.Text = "Изделия";
+            this.изделияToolStripMenuItem.Click += new System.EventHandler(this.изделияToolStripMenuItem_Click);
             // 
             // клиентыToolStripMenuItem
             // 
             this.клиентыToolStripMenuItem.Name = "клиентыToolStripMenuItem";
             this.клиентыToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.клиентыToolStripMenuItem.Text = "Клиенты";
+            this.клиентыToolStripMenuItem.Click += new System.EventHandler(this.клиентыToolStripMenuItem_Click);
+            // 
+            // buttonUpd
+            // 
+            this.buttonUpd.Location = new System.Drawing.Point(595, 54);
+            this.buttonUpd.Name = "buttonUpd";
+            this.buttonUpd.Size = new System.Drawing.Size(157, 23);
+            this.buttonUpd.TabIndex = 11;
+            this.buttonUpd.Text = "Обновить";
+            this.buttonUpd.UseVisualStyleBackColor = true;
+            this.buttonUpd.Click += new System.EventHandler(this.buttonUpd_Click);
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1472, 447);
+            this.ClientSize = new System.Drawing.Size(765, 447);
+            this.Controls.Add(this.buttonUpd);
             this.Controls.Add(this.buttonCreateOrder);
             this.Controls.Add(this.dataGridView);
             this.Controls.Add(this.menuStrip2);
             this.Name = "FormMain";
             this.Text = "FormMain";
+            this.Load += new System.EventHandler(this.FormMain_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
             this.menuStrip2.ResumeLayout(false);
             this.menuStrip2.PerformLayout();
@@ -118,8 +135,9 @@
         private System.Windows.Forms.DataGridView dataGridView;
         private System.Windows.Forms.MenuStrip menuStrip2;
         private System.Windows.Forms.ToolStripMenuItem справочникToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem заготовкиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem КомпонентыToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem изделияToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem клиентыToolStripMenuItem;
+        private System.Windows.Forms.Button buttonUpd;
     }
 }
--- ITShopBusinessLogic/ITShopWindowsApp/FormMain.cs
+++ ITShopBusinessLogic/ITShopWindowsApp/FormMain.cs
@@ -1,20 +1,81 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ITShopBusinessLogic.BusinessLogic;
+using ITShopBusinessLogic.Interfaces;
+using ITShopWindowsApp.Orders;
+using ITShopWindowsApp.Client;
+using ITShopWindowsApp.Product;
+using ITShopWindowsApp.Component;
+using System;
 using System.Windows.Forms;
+using Unity;
 
 namespace ITShopWindowsApp
 {
     public partial class FormMain : Form
     {
-        public FormMain()
+        [Dependency]
+        public new IUnityContainer Container { get; set; }
+        private readonly MainLogic logic;
+        private readonly IOrderLogic orderLogic;
+        public FormMain(MainLogic logic, IOrderLogic orderLogic)
         {
             InitializeComponent();
+            this.logic = logic;
+            this.orderLogic = orderLogic;
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            try
+            {
+                var list = orderLogic.Read(null);
+                if (list != null)
+                {
+                    dataGridView.DataSource = list;
+                    dataGridView.Columns[0].Visible = false;
+                    dataGridView.Columns[1].Visible = false;
+                    dataGridView.Columns[2].Visible = false;
+                    dataGridView.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void buttonCreateOrder_Click(object sender, EventArgs e)
+        {
+            var form = Container.Resolve<FormCreateOrder>();
+            form.ShowDialog(); 
+            LoadData();
+        }
+
+        private void КомпонентыToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var form = Container.Resolve<FormComponents>();
+            form.ShowDialog();
+        }
+
+        private void изделияToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var form = Container.Resolve<FormProduct>();
+            form.ShowDialog();
+        }
+
+        private void клиентыToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var form = Container.Resolve<FormClient>();
+            form.ShowDialog();
+        }
+
+        private void buttonUpd_Click(object sender, EventArgs e)
+        {
+            LoadData();
         }
     }
 }
