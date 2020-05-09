751f1dc91ec5fabbfc4b5deeb9ca230073b5ca38
KDANext <dima.1998.gig@mail.ru>
2020-05-09 18:24:07 +0400

    Update CreateOrderBindingModel.cs

--- ITShopBusinessLogic/ITShopBusinessLogic/BindingModels/CreateOrderBindingModel.cs
+++ ITShopBusinessLogic/ITShopBusinessLogic/BindingModels/CreateOrderBindingModel.cs
@@ -7,7 +7,7 @@ namespace ITShopBusinessLogic.BindingModels
     public class CreateOrderBindingModel
     {
         public int ClientId { get; set; }
-        public int ProductID { get; set; }
+        public int ProductId { get; set; }
         public int Count { get; set; }
         public decimal Sum { get; set; }
         public DateTime DateCreate { get; set; }
